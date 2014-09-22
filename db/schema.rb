afd53b7c2ffe3f7844b231b79a88e1c9a4f6d359
ashwinv11 <ashwinvaswani@alum.calarts.edu>
2014-09-22 13:03:21 -0700

    Hooked songs to playlists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140922063229) do
+ActiveRecord::Schema.define(version: 20140922192317) do
 
   create_table "playlists", force: true do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140922063229) do
     t.string   "genre"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "playlist_id"
   end
 
 end
