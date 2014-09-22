18a92695376f6c2dc6fdf669ebc4b7f9466dd87f
ashwinv11 <ashwinvaswani@alum.calarts.edu>
2014-09-21 23:42:05 -0700

    Added rails_12factor gem and updated Gemfile.lock

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915203754) do
+ActiveRecord::Schema.define(version: 20140922063229) do
 
   create_table "playlists", force: true do |t|
     t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "songs", force: true do |t|
+    t.string   "title"
+    t.string   "artist"
+    t.string   "album"
+    t.string   "time"
+    t.string   "genre"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
